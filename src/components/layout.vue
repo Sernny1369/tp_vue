f1708e7c836b297c1b0c31349a4baa9e4af3e64e
BabyNewCode <154010779+BabyNewCode@users.noreply.github.com>
2024-02-12 23:43:35 +0100

    Update CSS layout.vue

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -8,3 +8,16 @@ export default {
 }
 
 </script>
+
+<style scoped>
+.base-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Au moins la hauteur de la fenêtre visible */
+}
+
+main {
+  flex-grow: 1; /* Le contenu principal prendra l'espace disponible restant */
+}
+
+</style>
