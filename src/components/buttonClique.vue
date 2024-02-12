a26a50ef4b933c9a29989f3d28a3ab9196c4e686
Sernny1369 <135222155+Sernny1369@users.noreply.github.com>
2024-02-12 23:41:40 +0100

    Add files via upload

---
+++ src/components/buttonClique.vue
@@ -0,0 +1,24 @@
+<template>
+	<button lass="fency-btn">
+		<slot></slot>
+	</button>
+</template>
+            
+<script>
+export default {
+    name: 'BaseButton',
+}
+
+</script>
+
+<style>
+button {
+  color: #fff;
+  background: linear-gradient(315deg, #42d392 25%, #647eff);
+  border: none;
+  padding: 5px 10px;
+  margin: 5px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
